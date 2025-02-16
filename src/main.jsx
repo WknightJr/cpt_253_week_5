c56170b5ff1f66662544ebf3ae86a5b3795c92b9
= <=>
2025-02-16 16:06:22 -0500

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+
+// Pages
+import App from "./app";
+import { HomePage } from './pages/home';
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
+);
