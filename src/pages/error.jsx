8789ff0e5cbed31be3998a1b66b45129c588491f
WknightJr <austinbouchard6@gmail.com>
2025-02-22 19:14:38 -0500

    Update error.jsx

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import { Link } from "react-router-dom";
+//Here I import a link component//
 
 export const ErrorPage = () => {   
 
     return (
         <>
         <h1>You have received an error</h1>
+
+        <Link to="/">Home from link</Link>
         </>
     )
-
 }
+//This is a link to the home page//
