3fcd8dfaf21b281ad670c86de872833e8a4b9922
WknightJr <austinbouchard6@gmail.com>
2025-02-22 19:14:05 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,30 @@ import { BrowserRouter, Routes, Route } from "react-router";
 // Pages
 import App from "./app";
 import { HomePage } from './pages/home';
+import { ProfilePage } from './pages/profile'
+import { ErrorPage } from './pages/error'
+//I have imported the profile and error pages here//
+
 
 const root = document.getElementById("root");
 
+const router = BrowserRouter([{
+  path: '/',
+  element: <HomePage />,
+  errorElement: <ErrorPage />,
+},
+{
+  path: '/profiles',
+  element: <ProfilePage />,
+}
+//These are the router links to the profile and error pages//
+
+
+
+]);
+
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
